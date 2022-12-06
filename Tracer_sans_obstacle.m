b68d0c003cee715817d0009f19350f060e7141e9
octa-hexa <nezar.elmessnaoui@gmail.com>
2022-12-06 11:55:34 +0100

    final commit

--- Tracer_sans_obstacle.m
+++ Tracer_sans_obstacle.m
@@ -15,16 +15,37 @@ for i=1:M
     for j=1:N
         U_xy(i,j)= sqrt(U_x(i,j)^2+U_y(i,j)^2);
     end
+<<<<<<< HEAD
+ end
+ 
+ X = (0:L/(M-1):L);
+ Y = (0:D/(N-1):D);
+ 
+figure(1); surfc( X, Y , U_xy.'); title('Profil de la vitesse');xlabel('x (en m)'); ylabel('y (en m)'); shading interp; 
+h1 = colorbar;
+set(get(h1,'label'),'string','Vitesse u (en m/s)');
+figure(2); surfc( X, Y , Pr.'); title('Profil de la pression');xlabel('x (en m)'); ylabel('y (en m)'); shading flat;
+h2 = colorbar;
+set(get(h2,'label'),'string','Pression P (en Pa)');
+
+=======
 end
 X = (0:L/(M-1):L);Y = (0:D/(N-1):D);
 figure(1); surfc( X, Y , U_xy.'); title('Norme de Vitesse en (SI)');xlabel('Longeur X'); ylabel('Hauteur Y'); shading interp; colorbar;
 figure(2); surfc( X, Y , Pr.'); title('Pression en (SI)');xlabel('Longeur X'); ylabel('Hauteur Y'); shading flat; colorbar;
+>>>>>>> c6d99e365f224855fc6f41c7c4e01a831e67b8e6
 %Vitesse theorie
 U_t = -(Pg - Pd)*(Y - D).*Y*(1/(2*eta*L));
-figure(3);plot(Y,U_t);title('Vitesse theorique pour x donnée');xlabel('Hauteur Y'); ylabel('Vitesse');
+figure(3);plot(Y,U_t);title('Vitesse theorique en fonction de y');xlabel('y (en m)'); ylabel('Vitesse (en m/s)');
 x0 = 10; % l'abscisse de calcul
+<<<<<<< HEAD
+figure(4);plot(Y,U_xy(x0 , :));title('Vitesse numerique en fonction de y');xlabel('y (en m)'); ylabel('u (en m/s)');
+
+%Test de validation 
+=======
 figure(4);plot(Y,U_xy(x0 , :));title('Vitesse calculée pour x donnée');xlabel('Hauteur Y'); ylabel('Vitesse');
 %Test de validation
+>>>>>>> c6d99e365f224855fc6f41c7c4e01a831e67b8e6
 [IsValid , ErrM, ErrM1, ErrM2] = Validate_StokesEq(U_x , U_y , Pr);
 disp(['Erreur de validation : ', num2str(IsValid) ]);
 %Debit
@@ -33,4 +54,4 @@ for k=1:M
     Q = mean(U_xy(k,:)) * D;
     DQ(k)=Q;
 end
-figure(4);plot(X,DQ);title('Debit calculée');xlabel('Longeur X'); ylabel('Debit');axis([ 0 L 0 1e-2 ]);
+figure(5);plot(X,DQ);title('Debit calcule');xlabel('Longeur X'); ylabel('Debit');axis([ 0 L 0 1e-2 ]);
